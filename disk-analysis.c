4bd0e184de6765e25632351b2bb3a2b4d77f9cfa
Stephen D. Strowes <sdstrowes@gmail.com>
2017-08-13 21:52:34 +0200

    start being smarter about storing track data

--- disk-analysis.c
+++ disk-analysis.c
@@ -10,6 +10,8 @@
 #include "stream.h"
 #include "disk-analysis-log.h"
 
+#include "queue.h"
+
 #define TRACK_MAX 84
 #define SIDES      2
 
@@ -19,6 +21,10 @@ void print_help(char *binary_name)
 	printf("usage: %s -n basename\n", binary_name);
 }
 
+struct track_data {
+	struct track t;
+	STAILQ_ENTRY(track_data) next;
+};
 
 int main(int argc, char *argv[])
 {
@@ -42,13 +48,12 @@ int main(int argc, char *argv[])
 
 	log_init("", log_level);
 
-	struct track *track;
-	track = (struct track *)malloc(sizeof(struct track)*TRACK_MAX*SIDES);
-	if (track == NULL) {
-		log_err("Could not create track buffer");
-		return -1;
-	}
-	memset(track, 0, sizeof(struct track)*TRACK_MAX*SIDES);
+
+
+	STAILQ_HEAD(track_list, track_data) head = STAILQ_HEAD_INITIALIZER(head);
+	STAILQ_INIT(&head);
+
+	struct track_data *track;
 
 
 	if (fn_prefix == NULL) {
@@ -61,68 +66,38 @@ int main(int argc, char *argv[])
 	char *fn;
 	for (track_num = 0; track_num < TRACK_MAX; track_num++) {
 		for (side = 0; side < SIDES; side++) {
+			track = malloc(sizeof(struct track_data));
 			fn = (char *)malloc(strlen(fn_prefix) + 8 + 1);
 			sprintf(fn, "%s%02u.%u.raw", fn_prefix, track_num, side);
-			int rc = parse_stream(fn, &track[side ? track_num + TRACK_MAX : track_num], side, track_num);
+			int rc = parse_stream(fn, &track->t, side, track_num);
 			if (!rc) {
 				log_dbg("Loaded %s", fn);
+				STAILQ_INSERT_TAIL(&head, track, next);
 			}
 			else {
 				log_dbg("Error reading %s", fn);
+				free(track);
 			}
 			free(fn);
 		}
 	}
 
-	for (track_num = 0; track_num < TRACK_MAX; track_num++) {
-		for (side = 0; side < SIDES; side++) {
-			decode_stream(&track[side ? track_num + TRACK_MAX : track_num]);
-		}
+	STAILQ_FOREACH(track, &head, next) {
+		decode_stream(&track->t);
 	}
 
-//	struct colour *img_buffer;
-//	uint16_t width  = 4200;
-//	uint16_t height = 4200;
-
-//	img_buffer = (struct colour *) malloc(width * height * sizeof(struct colour));
-//	if (img_buffer == NULL) {
-//		log_err("Could not create image buffer");
-//		return -1;
-//	}
-//	memset(img_buffer, 0, width * height * sizeof(struct colour));
-
-//	for (track = 0; track < TRACK_MAX; track++) {
-//		for (side = 0; side < SIDES; side++) {
-//			dump_stream(&track[(side*track)+track]);
-//		}
-//	}
-
-//	for (track_num = 0; track_num < 82; track_num++) {
-//		//for (side = 0; side < SIDES; side++) {
-//			plot_track(&track[side ? track_num + TRACK_MAX : track_num], img_buffer, width, height);
-//		//}
-//	}
-//
-//	dump_stream_img(img_buffer, width, height);
-
-//	free(img_buffer); img_buffer = NULL;
 	FILE *svg_out = test_svg_out();
-	//for (track_num = 0; track_num < TRACK_MAX; track_num++) {
-	for (track_num = 0; track_num < 82; track_num++) {
-		for (side = 0; side < SIDES; side++) {
-			plot_track(svg_out, &track[side ? track_num + TRACK_MAX : track_num]);
-		}
+	STAILQ_FOREACH(track, &head, next) {
+		plot_track(svg_out, &track->t);
 	}
+
 	finalise_svg(svg_out);
 
-	for (track_num = 0; track_num < TRACK_MAX; track_num++) {
-		for (side = 0; side < SIDES; side++) {
-			free_stream(&track[side ? track_num + TRACK_MAX : track_num]);
-		}
+	while (!STAILQ_EMPTY(&head)) {
+		track = STAILQ_FIRST(&head);
+		STAILQ_REMOVE_HEAD(&head, next);
+		free(track);
 	}
-	free(track);
-
-
 
 	return 0;
 }
